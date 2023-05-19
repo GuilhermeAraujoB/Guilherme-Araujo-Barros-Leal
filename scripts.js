6cdc640c4aaca4adc2c7aee89e68b2eeb70b12bc
GuilhermeAraujoB <105887310+GuilhermeAraujoB@users.noreply.github.com>
2023-05-19 08:34:06 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const cards = document.querySelectorAll('.card');//variavel que procura e guarda
 
 for (let card of cards){
     card.addEventListener("click",function(){
-        modal_overlay.classList.remove("active")
+        modal_overlay.classList.add("active")
     })
 }
 
